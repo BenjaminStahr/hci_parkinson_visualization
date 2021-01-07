4add488042e2238429b4caf2b9f4e876b18eb903
BenjaminStahr <BenjaminStahr97@web.de>
2021-01-07 20:30:33 +0100

    added navigation for app trough gamemanager

---
+++ ParkinsonMiniProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public enum AppState
+{
+    Information,
+    DBS
+}
+
+public class GameManager : MonoBehaviour
+{
+    public AppState current;
+    public GameObject ModusButton;
+    public GameObject GlobusButton;
+    public GameObject SubtantiaButton;
+    public GameObject NucleusButton;
+    public GameObject GlobusDBSButton;
+    public GameObject NucleusDBSButton;
+    public GameObject InfoText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = AppState.Information;
+        ModusButton = GameObject.FindGameObjectWithTag("Modus");
+        GlobusButton = GameObject.FindGameObjectWithTag("Globus");
+        SubtantiaButton = GameObject.FindGameObjectWithTag("Subtantia");
+        NucleusButton = GameObject.FindGameObjectWithTag("Nucleus");
+        GlobusDBSButton = GameObject.FindGameObjectWithTag("GlobusDBS");
+        NucleusDBSButton = GameObject.FindGameObjectWithTag("NucleusDBS");
+        InfoText = GameObject.FindGameObjectWithTag("InfoText");
+
+        GlobusDBSButton.SetActive(false);
+        NucleusDBSButton.SetActive(false);
+    }
+
+    void Update()
+    {
+        switch (current)
+        {
+            case AppState.Information:
+                NucleusButton.SetActive(true);
+                SubtantiaButton.SetActive(true);
+                GlobusButton.SetActive(true);
+                GlobusDBSButton.SetActive(false);
+                NucleusDBSButton.SetActive(false);
+                break;
+            case AppState.DBS:
+                NucleusButton.SetActive(false);
+                SubtantiaButton.SetActive(false);
+                GlobusButton.SetActive(false);
+                GlobusDBSButton.SetActive(true);
+                NucleusDBSButton.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+    public void SwitchMode()
+    {
+        if (current == AppState.Information)
+        {
+            current = AppState.DBS;
+            ModusButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "DBS";
+        }
+        else
+        {
+            current = AppState.Information;
+            ModusButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Information";
+        }
+    }
+
+    public void SetTextNucleus()
+    {
+        InfoText.GetComponent<TextMesh>().text = "Nucleus";
+    }
+    public void SetTextSubtantia()
+    {
+        InfoText.GetComponent<TextMesh>().text = "Subtantia";
+    }
+    public void SetTextGlobus()
+    {
+        InfoText.GetComponent<TextMesh>().text = "Globus";
+    }
+    public void SetTextNucleusDBS()
+    {
+        InfoText.GetComponent<TextMesh>().text = "Nucleus DBS";
+    }
+    public void SetTextGlobusDBS()
+    {
+        InfoText.GetComponent<TextMesh>().text = "Globus DBS";
+    }
+}
